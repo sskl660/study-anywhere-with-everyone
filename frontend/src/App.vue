37105797aa60b24986f146ac30204d990d4186d0
Kimtaehyun <sskl600@naver.com>
2021-07-29 01:27:51 +0900

    success pipeline!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">SSAZIP</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">YEAH!!!!!</router-link>
     </div>
     <router-view />
   </div>
