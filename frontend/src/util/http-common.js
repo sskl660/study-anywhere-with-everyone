77dd7ca099bd6f0e913d897493373cafc2dc4fe9
SEOPSS\prero <comet00096@gmail.com>
2021-08-04 01:53:10 +0900

    fix : server port

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 // axios 객체 생성
 export default axios.create({
-//baseURL: 'http://13.125.119.76:8080/',
-baseURL: 'http://localhost:8080/',
+    baseURL: 'http://13.125.119.76:8080/',
+    //baseURL: 'http://localhost:8080/',
 
-headers: {
-    'Content-type': 'application/json',
-},
+    headers: {
+        'Content-type': 'application/json',
+    },
 });
