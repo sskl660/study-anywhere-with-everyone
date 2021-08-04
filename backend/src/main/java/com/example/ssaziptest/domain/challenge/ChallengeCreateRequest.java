ac6ea2be894d25e8d80827df24e48a967e2dfc53
heewonyouh <67193692+heewonyouh@users.noreply.github.com>
2021-08-04 15:51:27 +0900

    update(Backend):change String to LocalDate

--- backend/src/main/java/com/example/ssaziptest/domain/challenge/ChallengeCreateRequest.java
+++ backend/src/main/java/com/example/ssaziptest/domain/challenge/ChallengeCreateRequest.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,26 +17,27 @@ public class ChallengeCreateRequest {
     private String challengeCategory;
     private int challengeLevel;
     private int challengeCapacity;
-    private String challengeStartdate;
-    private String challengeEnddate;
+    private LocalDate challengeStartdate;
+    private LocalDate challengeEnddate;
     private String challengeDesc;
     private int challengeTaskCnt;
-    private String challengeTaskdeadlines;
+    private LocalDate[] challengeTaskdeadlines;
 
 
     public ChallengeEntity toEntity(){
+
         ChallengeEntity challengeEntity = ChallengeEntity.builder()
                 .challengeName(challengeName).challengeCategory(challengeCategory)
                 .challengeLevel(challengeLevel).challengeCapacity(challengeCapacity).challengeStartdate(challengeStartdate)
                 .challengeEnddate(challengeEnddate).challengeDesc(challengeDesc).challengeTaskCnt(challengeTaskCnt)
-                .challengeTaskdeadlines(Arrays.asList(challengeTaskdeadlines.split(",")))
+                .challengeTaskdeadlines(Arrays.asList(challengeTaskdeadlines))
                 .build();
         return challengeEntity;
     }
 
     @Builder
 
-    public ChallengeCreateRequest(String challengeName, String challengeCategory, int challengeLevel, int challengeCapacity, String challengeStartdate, String challengeEnddate, String challengeDesc, int challengeTaskCnt, String challengeTaskdeadlines) {
+    public ChallengeCreateRequest(String challengeName, String challengeCategory, int challengeLevel, int challengeCapacity, LocalDate challengeStartdate, LocalDate challengeEnddate, String challengeDesc, int challengeTaskCnt, LocalDate[] challengeTaskdeadlines) {
         this.challengeName = challengeName;
         this.challengeCategory = challengeCategory;
         this.challengeLevel = challengeLevel;
