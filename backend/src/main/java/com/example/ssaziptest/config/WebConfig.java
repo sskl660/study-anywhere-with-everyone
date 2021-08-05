bb6cbc772066c10b557433dd8557c0ec6a2e37c0
taehyunKim <sskl600@naver.com>
2021-08-05 19:50:17 +0900

    fix(CORS) : fix CORS rule

--- backend/src/main/java/com/example/ssaziptest/config/WebConfig.java
+++ backend/src/main/java/com/example/ssaziptest/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://13.125.119.76:3000", "http://localhost:3000")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
