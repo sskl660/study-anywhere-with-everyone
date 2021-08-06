f07c7118012822e55f24dcb418322d6cd4dd920e
heeeunk <heeeunk777@gmail.com>
2021-08-06 14:31:12 +0900

    update(Profile)&update(Nav): link login user with profile and add user on the top of the web

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -4,8 +4,9 @@
     <div>
       <Logout/>
     </div>
-    <div style="position:relative; z-index:2;">
-      <Navbar/>
+    <div class="d-flex" style="position:relative; z-index:2; width:1750px">
+      <div class="greeting" style="display:inline-block;">{{this.userName}}님 환영합니다</div>
+      <Navbar style="display:inline-block"/>
     </div>
   </div>
 </template>
@@ -13,16 +14,32 @@
 <script>
 import Navbar from '@/components/header/Navbar.vue'
 import Logout from '@/components/header/Logout.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Header',
   components: {
     Navbar,
     Logout,
-  }
+  },
+  computed: {
+    ...mapState([
+      'userName',
+    ])
+  },
 }
 </script>
 
 <style>
- 
+.greeting {
+  position:absolute;
+  top: 40px;
+  width: 260px;
+  height: 45px;
+  background-color: rgb(31, 66, 86, 0.7);
+  color:white; 
+  font-size:25px;
+  line-height:50px;
+  border-radius: 1rem;
+}
 </style>
