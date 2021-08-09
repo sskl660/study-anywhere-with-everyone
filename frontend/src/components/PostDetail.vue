974c5182585b49846a69902bda04584c855693dd
heeeunk <heeeunk777@gmail.com>
2021-08-09 19:15:22 +0900

    update(Profile):upload image

--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -86,19 +86,11 @@
             </div>
 
             <div style="margin-left:907px; margin-top:7px;" class="d-flex">
-<<<<<<< HEAD
                 <button class="btn btn-danger d-flex align-items-center" @click="goBack()">
                     <!-- <router-link to="/ChallengeRoom" style="text-decoration: none; color: #ffffff"> -->
                     <div style="text-decoration: none; color: #ffffff">취소</div>
                     <!-- </router-link> -->
                 </button>&nbsp;
-=======
-                <button class="btn btn-danger d-flex align-items-center">
-                    <router-link to="/ChallengeRoom" style="text-decoration: none; color: #ffffff">
-                        <div>취소</div>
-                    </router-link></button
-                >&nbsp;
->>>>>>> 85d6355022a26eb2b3e75d4988194eb44fbadcf9
                 <button class="btn btn-primary d-flex align-items-center" @click="sendPost">
                     <div>생성</div>
                 </button>
